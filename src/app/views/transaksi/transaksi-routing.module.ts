10e76c848922cf9d9bee947c45193171af8f159d
Dicky Muzakki <dickymuzakki05@gmail.com>
2024-09-23 18:02:56 +0700

    new

--- src/app/views/transaksi/transaksi-routing.module.ts
+++ src/app/views/transaksi/transaksi-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewHeaderMoComponent } from './view-header-mo/view-header-mo.component';
-import { AddHeaderMoComponent } from './add-header-mo/add-header-mo.component';
 
 const routes: Routes = [{
 
@@ -19,13 +18,6 @@ const routes: Routes = [{
       component: ViewHeaderMoComponent,
       data: {
         title: 'View Header Monthly Planning'
-      }
-    },
-    {
-      path: 'transaksi/add-header-mo',
-      component: AddHeaderMoComponent,
-      data: {
-        title: 'Add Header Monthly Planning'
       }
     }
   ]
