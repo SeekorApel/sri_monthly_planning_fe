e9317aa7cd33af69b4db9b1c934b30d1d0c64ac8
Dicky Muzakki <dickymuzakki05@gmail.com>
2024-10-18 14:29:22 +0700

    new

--- src/app/views/transaksi/add-mo-marketing/add-mo-marketing.component.ts
+++ src/app/views/transaksi/add-mo-marketing/add-mo-marketing.component.ts
@@ -129,44 +129,44 @@ export class AddMoMarketingComponent implements OnInit {
     this.headerMarketingOrder = data.dataHeaderMo;
     this.detailMarketingOrder = data.dataDetailMo;
 
-    // this.formHeaderMo.patchValue({
-    //   date: new Date(data.datevalid).toISOString().split('T')[0],
-    //   type: data.type,
-    //   revision: data.revision,
+    this.formHeaderMo.patchValue({
+      date: new Date(data.dateValid).toISOString().split('T')[0],
+      type: data.type,
+      revision: data.revision,
 
-    //   // Header Month 1
-    //   month_0: this.formatDateToString(this.headerMarketingOrder[1].month),
-    //   nwd_0: this.headerMarketingOrder[1].wd_NORMAL,
-    //   tl_ot_wd_0: this.headerMarketingOrder[1].wd_OT_TL,
-    //   tt_ot_wd_0: this.headerMarketingOrder[1].wd_OT_TT,
-    //   total_tlwd_0: this.headerMarketingOrder[1].total_WD_TL,
-    //   total_ttwd_0: this.headerMarketingOrder[1].total_WD_TT,
-    //   max_tube_capa_0: this.headerMarketingOrder[1].max_CAP_TUBE,
-    //   max_capa_tl_0: this.headerMarketingOrder[1].max_CAP_TL,
-    //   max_capa_tt_0: this.headerMarketingOrder[1].max_CAP_TT,
+      // Header Month 1
+      month_0: this.formatDateToString(this.headerMarketingOrder[1].month),
+      nwd_0: this.headerMarketingOrder[1].wdNormal,
+      tl_ot_wd_0: this.headerMarketingOrder[1].wdOtTl,
+      tt_ot_wd_0: this.headerMarketingOrder[1].wdOtTt,
+      total_tlwd_0: this.headerMarketingOrder[1].totalWdTl,
+      total_ttwd_0: this.headerMarketingOrder[1].totalWdTt,
+      max_tube_capa_0: this.headerMarketingOrder[1].maxCapTube,
+      max_capa_tl_0: this.headerMarketingOrder[1].maxCapTl,
+      max_capa_tt_0: this.headerMarketingOrder[1].maxCapTt,
 
-    //   // Header Month 2
-    //   month_1: this.formatDateToString(this.headerMarketingOrder[2].month),
-    //   nwd_1: this.headerMarketingOrder[2].wd_NORMAL,
-    //   tl_ot_wd_1: this.headerMarketingOrder[2].wd_OT_TL,
-    //   tt_ot_wd_1: this.headerMarketingOrder[2].wd_OT_TT,
-    //   total_tlwd_1: this.headerMarketingOrder[2].total_WD_TL,
-    //   total_ttwd_1: this.headerMarketingOrder[2].total_WD_TT,
-    //   max_tube_capa_1: this.headerMarketingOrder[2].max_CAP_TUBE,
-    //   max_capa_tl_1: this.headerMarketingOrder[2].max_CAP_TL,
-    //   max_capa_tt_1: this.headerMarketingOrder[2].max_CAP_TT,
+      // Header Month 2
+      month_1: this.formatDateToString(this.headerMarketingOrder[2].month),
+      nwd_1: this.headerMarketingOrder[2].wdNormal,
+      tl_ot_wd_1: this.headerMarketingOrder[2].wdOtTl,
+      tt_ot_wd_1: this.headerMarketingOrder[2].wdOtTt,
+      total_tlwd_1: this.headerMarketingOrder[2].totalWdTl,
+      total_ttwd_1: this.headerMarketingOrder[2].totalWdTt,
+      max_tube_capa_1: this.headerMarketingOrder[2].maxCapTube,
+      max_capa_tl_1: this.headerMarketingOrder[2].maxCapTl,
+      max_capa_tt_1: this.headerMarketingOrder[2].maxCapTt,
 
-    //   // Header Month 3
-    //   month_2: this.formatDateToString(this.headerMarketingOrder[0].month),
-    //   nwd_2: this.headerMarketingOrder[0].wd_NORMAL,
-    //   tl_ot_wd_2: this.headerMarketingOrder[0].wd_OT_TL,
-    //   tt_ot_wd_2: this.headerMarketingOrder[0].wd_OT_TT,
-    //   total_tlwd_2: this.headerMarketingOrder[0].total_WD_TL,
-    //   total_ttwd_2: this.headerMarketingOrder[0].total_WD_TT,
-    //   max_tube_capa_2: this.headerMarketingOrder[0].max_CAP_TUBE,
-    //   max_capa_tl_2: this.headerMarketingOrder[0].max_CAP_TL,
-    //   max_capa_tt_2: this.headerMarketingOrder[0].max_CAP_TT,
-    // });
+      // Header Month 3
+      month_2: this.formatDateToString(this.headerMarketingOrder[0].month),
+      nwd_2: this.headerMarketingOrder[0].wdNormal,
+      tl_ot_wd_2: this.headerMarketingOrder[0].wdOtTl,
+      tt_ot_wd_2: this.headerMarketingOrder[0].wdOtTt,
+      total_tlwd_2: this.headerMarketingOrder[0].totalWdTl,
+      total_ttwd_2: this.headerMarketingOrder[0].totalWdTt,
+      max_tube_capa_2: this.headerMarketingOrder[0].maxCapTube,
+      max_capa_tl_2: this.headerMarketingOrder[0].maxCapTl,
+      max_capa_tt_2: this.headerMarketingOrder[0].maxCapTt,
+    });
 
     this.updateMonthNames(this.headerMarketingOrder);
   }
--- src/app/views/transaksi/edit-mo-ppc/edit-mo-ppc.component.ts
+++ src/app/views/transaksi/edit-mo-ppc/edit-mo-ppc.component.ts
@@ -6,20 +6,27 @@ import * as ExcelJS from 'exceljs/dist/exceljs.min.js';
 import * as XLSX from 'xlsx';
 import { saveAs } from 'file-saver';
 import { DetailMarketingOrder } from 'src/app/models/DetailMarketingOrder';
+import { MarketingOrderService } from 'src/app/services/transaksi/marketing order/marketing-order.service';
+import { ApiResponse } from 'src/app/response/Response';
+import { MarketingOrder } from 'src/app/models/MarketingOrder';
+import { HeaderMarketingOrder } from 'src/app/models/HeaderMarketingOrder';
 
 @Component({
   selector: 'app-edit-mo-ppc',
   templateUrl: './edit-mo-ppc.component.html',
-  styleUrls: ['./edit-mo-ppc.component.scss']
+  styleUrls: ['./edit-mo-ppc.component.scss'],
 })
 export class EditMoPpcComponent implements OnInit {
-
   //Variable Declaration
   idMo: String;
   formHeaderMo: FormGroup;
   isReadOnly: boolean = true;
   monthNames: string[] = ['', '', ''];
-  marketingOrderTable: DetailMarketingOrder[];
+  allData: any;
+
+  marketingOrder: MarketingOrder;
+  headerMarketingOrder: HeaderMarketingOrder[];
+  detailMarketingOrder: DetailMarketingOrder[];
 
   //Workday
   workDay_M0: any[] = [];
@@ -45,44 +52,74 @@ export class EditMoPpcComponent implements OnInit {
     total_ot_tt: 0,
   };
 
-  constructor(private router: Router, private activeRoute: ActivatedRoute, private fb: FormBuilder) {
+  constructor(private router: Router, private activeRoute: ActivatedRoute, private fb: FormBuilder, private moService: MarketingOrderService) {
     this.formHeaderMo = this.fb.group({
-      date: ['', Validators.required],
-      type: ['', Validators.required],
-      revision: ['', []],
-      month_0: ['', Validators.required],
-      month_1: ['', []],
-      month_2: ['', []],
-      nwd_0: ['', Validators.required],
-      nwd_1: ['', Validators.required],
-      nwd_2: ['', Validators.required],
-      tl_ot_wd_0: ['', [Validators.required, Validators.min(0)]],
-      tt_ot_wd_0: ['', [Validators.required, Validators.min(0)]],
-      tl_ot_wd_1: ['', [Validators.required, Validators.min(0)]],
-      tt_ot_wd_1: ['', [Validators.required, Validators.min(0)]],
-      tl_ot_wd_2: ['', [Validators.required, Validators.min(0)]],
-      tt_ot_wd_2: ['', [Validators.required, Validators.min(0)]],
-      total_tlwd_0: ['', []],
-      total_ttwd_0: ['', []],
-      total_tlwd_1: ['', []],
-      total_ttwd_1: ['', []],
-      total_tlwd_2: ['', []],
-      total_ttwd_2: ['', []],
-      max_tube_capa_0: [10, [Validators.required, Validators.min(0)]],
-      max_tube_capa_1: [20, [Validators.required, Validators.min(0)]],
-      max_tube_capa_2: [30, [Validators.required, Validators.min(0)]],
-      max_capa_tl_0: [10, [Validators.required, Validators.min(0)]],
-      max_capa_tt_0: [20, [Validators.required, Validators.min(0)]],
-      max_capa_tl_1: [30, [Validators.required, Validators.min(0)]],
-      max_capa_tt_1: [10, [Validators.required, Validators.min(0)]],
-      max_capa_tl_2: [20, [Validators.required, Validators.min(0)]],
-      max_capa_tt_2: [30, [Validators.required, Validators.min(0)]],
-      looping_m0:['',[]],
-      machine_airbag_m0:['',[]],
-      fed_tl_m0:['',[]],
-      fed_tt_m0:['',[]],
-      total_mo_m0:['',[]],
-      note_tl_m0:['',[]],
+      date: [null, Validators.required],
+      type: [null, Validators.required],
+      revision: [null, []],
+      month_0: [null, Validators.required],
+      month_1: [null, []],
+      month_2: [null, []],
+      nwd_0: [null, Validators.required],
+      nwd_1: [null, Validators.required],
+      nwd_2: [null, Validators.required],
+      tl_ot_wd_0: [null, [Validators.required, Validators.min(0)]],
+      tt_ot_wd_0: [null, [Validators.required, Validators.min(0)]],
+      tl_ot_wd_1: [null, [Validators.required, Validators.min(0)]],
+      tt_ot_wd_1: [null, [Validators.required, Validators.min(0)]],
+      tl_ot_wd_2: [null, [Validators.required, Validators.min(0)]],
+      tt_ot_wd_2: [null, [Validators.required, Validators.min(0)]],
+      total_tlwd_0: [null, []],
+      total_ttwd_0: [null, []],
+      total_tlwd_1: [null, []],
+      total_ttwd_1: [null, []],
+      total_tlwd_2: [null, []],
+      total_ttwd_2: [null, []],
+      max_tube_capa_0: [null, [Validators.required, Validators.min(0)]],
+      max_tube_capa_1: [null, [Validators.required, Validators.min(0)]],
+      max_tube_capa_2: [null, [Validators.required, Validators.min(0)]],
+      max_capa_tl_0: [null, [Validators.required, Validators.min(0)]],
+      max_capa_tt_0: [null, [Validators.required, Validators.min(0)]],
+      max_capa_tl_1: [null, [Validators.required, Validators.min(0)]],
+      max_capa_tt_1: [null, [Validators.required, Validators.min(0)]],
+      max_capa_tl_2: [null, [Validators.required, Validators.min(0)]],
+      max_capa_tt_2: [null, [Validators.required, Validators.min(0)]],
+      looping_m0: [null, []],
+      machine_airbag_m0: [null, []],
+      fed_tl_m0: [null, []],
+      fed_tt_m0: [null, []],
+      fdr_tl_m0: [null, []],
+      fdr_tt_m0: [null, []],
+      total_mo_m0: [null, []],
+      fed_TL_percentage_m0: [null, []],
+      fdr_TL_percentage_m0: [null, []],
+      fed_TT_percentage_m0: [null, []],
+      fdr_TT_percentage_m0: [null, []],
+      note_tl_m0: [null, []],
+      looping_m1: [null, []],
+      machine_airbag_m1: [null, []],
+      fed_tl_m1: [null, []],
+      fed_tt_m1: [null, []],
+      fdr_tl_m1: [null, []],
+      fdr_tt_m1: [null, []],
+      total_mo_m1: [null, []],
+      fed_TL_percentage_m1: [null, []],
+      fdr_TL_percentage_m1: [null, []],
+      fed_TT_percentage_m1: [null, []],
+      fdr_TT_percentage_m1: [null, []],
+      note_tl_m1: [null, []],
+      looping_m2: [null, []],
+      machine_airbag_m2: [null, []],
+      fed_tl_m2: [null, []],
+      fed_tt_m2: [null, []],
+      fdr_tl_m2: [null, []],
+      fdr_tt_m2: [null, []],
+      total_mo_m2: [null, []],
+      fed_TL_percentage_m2: [null, []],
+      fdr_TL_percentage_m2: [null, []],
+      fed_TT_percentage_m2: [null, []],
+      fdr_TT_percentage_m2: [null, []],
+      note_tl_m2: [null, []],
       upload_file_m0: [null, [Validators.required]],
       upload_file_m1: [null, [Validators.required]],
       upload_file_m2: [null, [Validators.required]],
@@ -91,7 +128,127 @@ export class EditMoPpcComponent implements OnInit {
 
   ngOnInit(): void {
     this.idMo = this.activeRoute.snapshot.paramMap.get('idMo');
-    console.log("Ini id mo edit", this.idMo);
+    this.getAllData(this.idMo);
+  }
+
+  editMo(): void {}
+
+  getAllData(idMo: String) {
+    this.moService.getDetailMarketingOrderPpc(idMo).subscribe(
+      (response: ApiResponse<any>) => {
+        this.allData = response.data;
+        this.fillAllData(this.allData);
+      },
+      (error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Failed to load marketing order details: ' + error.message,
+          confirmButtonText: 'OK',
+        });
+      }
+    );
+  }
+
+  fillAllData(data: any) {
+    this.headerMarketingOrder = data.dataHeaderMo;
+    this.detailMarketingOrder = data.dataDetailMo;
+    let typeProduct = data.type;
+
+    this.formHeaderMo.patchValue({
+      date: new Date(data.dateValid).toISOString().split('T')[0],
+      type: data.type,
+      revision: data.revisionPpc,
+
+      // Header Month 1
+      month_0: this.formatDateToString(this.headerMarketingOrder[1].month),
+      nwd_0: this.headerMarketingOrder[1].wdNormal,
+      tl_ot_wd_0: this.headerMarketingOrder[1].wdOtTl,
+      tt_ot_wd_0: this.headerMarketingOrder[1].wdOtTt,
+      total_tlwd_0: this.headerMarketingOrder[1].totalWdTl,
+      total_ttwd_0: this.headerMarketingOrder[1].totalWdTt,
+      max_tube_capa_0: this.headerMarketingOrder[1].maxCapTube,
+      max_capa_tl_0: this.headerMarketingOrder[1].maxCapTl,
+      max_capa_tt_0: this.headerMarketingOrder[1].maxCapTt,
+      looping_m0: this.headerMarketingOrder[1].looping,
+      machine_airbag_m0: this.headerMarketingOrder[1].airbagMachine,
+      fed_tl_m0: typeProduct === 'FED' ? this.headerMarketingOrder[1].tl : null,
+      fed_tt_m0: typeProduct === 'FED' ? this.headerMarketingOrder[1].tt : null,
+      fdr_tl_m0: typeProduct === 'FDR' ? this.headerMarketingOrder[1].tl : null,
+      fdr_tt_m0: typeProduct === 'FDR' ? this.headerMarketingOrder[1].tt : null,
+      fed_TL_percentage_m0: typeProduct === 'FED' ? this.headerMarketingOrder[1].tlPercentage : null,
+      fed_TT_percentage_m0: typeProduct === 'FED' ? this.headerMarketingOrder[1].ttPercentage : null,
+      fdr_TL_percentage_m0: typeProduct === 'FDR' ? this.headerMarketingOrder[1].tlPercentage : null,
+      fdr_TT_percentage_m0: typeProduct === 'FDR' ? this.headerMarketingOrder[1].ttPercentage : null,
+      total_mo_m0: this.headerMarketingOrder[1].totalMo,
+      note_tl_m0: this.headerMarketingOrder[1].noteOrderTl,
+      // Header Month 2
+      month_1: this.formatDateToString(this.headerMarketingOrder[2].month),
+      nwd_1: this.headerMarketingOrder[2].wdNormal,
+      tl_ot_wd_1: this.headerMarketingOrder[2].wdOtTl,
+      tt_ot_wd_1: this.headerMarketingOrder[2].wdOtTt,
+      total_tlwd_1: this.headerMarketingOrder[2].totalWdTl,
+      total_ttwd_1: this.headerMarketingOrder[2].totalWdTt,
+      max_tube_capa_1: this.headerMarketingOrder[2].maxCapTube,
+      max_capa_tl_1: this.headerMarketingOrder[2].maxCapTl,
+      max_capa_tt_1: this.headerMarketingOrder[2].maxCapTt,
+      looping_m1: this.headerMarketingOrder[2].looping,
+      machine_airbag_m1: this.headerMarketingOrder[2].airbagMachine,
+      fed_tl_m1: typeProduct === 'FED' ? this.headerMarketingOrder[2].tl : null,
+      fed_tt_m1: typeProduct === 'FED' ? this.headerMarketingOrder[2].tt : null,
+      fdr_tl_m1: typeProduct === 'FDR' ? this.headerMarketingOrder[2].tl : null,
+      fdr_tt_m1: typeProduct === 'FDR' ? this.headerMarketingOrder[2].tt : null,
+      fed_TL_percentage_m1: typeProduct === 'FED' ? this.headerMarketingOrder[2].tlPercentage : null,
+      fed_TT_percentage_m1: typeProduct === 'FED' ? this.headerMarketingOrder[2].ttPercentage : null,
+      fdr_TL_percentage_m1: typeProduct === 'FDR' ? this.headerMarketingOrder[2].tlPercentage : null,
+      fdr_TT_percentage_m1: typeProduct === 'FDR' ? this.headerMarketingOrder[2].ttPercentage : null,
+      total_mo_m1: this.headerMarketingOrder[2].totalMo,
+      note_tl_m1: this.headerMarketingOrder[2].noteOrderTl,
+      // Header Month 3
+      month_2: this.formatDateToString(this.headerMarketingOrder[0].month),
+      nwd_2: this.headerMarketingOrder[0].wdNormal,
+      tl_ot_wd_2: this.headerMarketingOrder[0].wdOtTl,
+      tt_ot_wd_2: this.headerMarketingOrder[0].wdOtTt,
+      total_tlwd_2: this.headerMarketingOrder[0].totalWdTl,
+      total_ttwd_2: this.headerMarketingOrder[0].totalWdTt,
+      max_tube_capa_2: this.headerMarketingOrder[0].maxCapTube,
+      max_capa_tl_2: this.headerMarketingOrder[0].maxCapTl,
+      max_capa_tt_2: this.headerMarketingOrder[0].maxCapTt,
+      looping_m2: this.headerMarketingOrder[0].looping,
+      machine_airbag_m2: this.headerMarketingOrder[0].airbagMachine,
+      fed_tl_m2: typeProduct === 'FED' ? this.headerMarketingOrder[2].tl : null,
+      fed_tt_m2: typeProduct === 'FED' ? this.headerMarketingOrder[2].tt : null,
+      fdr_tl_m2: typeProduct === 'FDR' ? this.headerMarketingOrder[2].tl : null,
+      fdr_tt_m2: typeProduct === 'FDR' ? this.headerMarketingOrder[2].tt : null,
+      fed_TL_percentage_m2: typeProduct === 'FED' ? this.headerMarketingOrder[2].tlPercentage : null,
+      fed_TT_percentage_m2: typeProduct === 'FED' ? this.headerMarketingOrder[2].ttPercentage : null,
+      fdr_TL_percentage_m2: typeProduct === 'FDR' ? this.headerMarketingOrder[2].tlPercentage : null,
+      fdr_TT_percentage_m2: typeProduct === 'FDR' ? this.headerMarketingOrder[2].ttPercentage : null,
+      total_mo_m2: this.headerMarketingOrder[0].totalMo,
+      note_tl_m2: this.headerMarketingOrder[0].noteOrderTl,
+    });
+
+    this.updateMonthNames(this.headerMarketingOrder);
+  }
+
+  formatDateToString(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    return `${year}-${month}`;
+  }
+
+  updateMonthNames(hm: HeaderMarketingOrder[]): void {
+    this.monthNames[0] = this.getMonthName(new Date(this.headerMarketingOrder[1].month));
+    this.monthNames[1] = this.getMonthName(new Date(this.headerMarketingOrder[2].month));
+    this.monthNames[2] = this.getMonthName(new Date(this.headerMarketingOrder[0].month));
+  }
+
+  getMonthName(monthValue: Date): string {
+    if (monthValue) {
+      return monthValue.toLocaleString('default', { month: 'short' }).toUpperCase();
+    }
+    return '';
   }
 
   downloadTemplate_M1() {
@@ -798,5 +955,4 @@ export class EditMoPpcComponent implements OnInit {
   navigateToViewMo() {
     this.router.navigate(['/transaksi/view-mo-ppc']);
   }
-
 }
